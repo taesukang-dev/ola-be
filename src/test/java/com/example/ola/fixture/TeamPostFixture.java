62adf6ba891490e2fd6d027fa6297218d90bbb35
mtae616 <mtae616@naver.com>
2022-11-05 22:31:09 +0900

    add fixtures for test

---
+++ src/test/java/com/example/ola/fixture/TeamPostFixture.java
@@ -0,0 +1,12 @@
+package com.example.ola.fixture;
+
+import com.example.ola.domain.TeamBuildingPost;
+import com.example.ola.domain.User;
+
+public class TeamPostFixture {
+
+    public static TeamBuildingPost makeFixture() {
+        User user = new User("user1", "password1", "nick1", "name1", 30L, "home");
+        return TeamBuildingPost.of(user, "title", "content", "place", 3L);
+    }
+}
