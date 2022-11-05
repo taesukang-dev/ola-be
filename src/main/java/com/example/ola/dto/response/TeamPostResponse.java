bea6d581aa0f99a96808a98c2aa4cc6966f527ad
mtae616 <mtae616@naver.com>
2022-11-05 22:30:08 +0900

    add team post response

---
+++ src/main/java/com/example/ola/dto/response/TeamPostResponse.java
@@ -0,0 +1,42 @@
+package com.example.ola.dto.response;
+
+import com.example.ola.dto.TeamPostDto;
+import lombok.Data;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TeamPostResponse {
+    private Long id;
+    private UserJoinResponse userJoinResponse;
+    private String title;
+    private String content;
+    private String place;
+    private Long limits;
+    private List<UserJoinResponse> member;
+    private Timestamp registeredAt;
+
+    public static TeamPostResponse fromTeamPostDto(TeamPostDto teamBuildingPost) {
+        return new TeamPostResponse(
+                teamBuildingPost.getId(),
+                UserJoinResponse.fromUserDto(teamBuildingPost.getUserDto()),
+                teamBuildingPost.getTitle(),
+                teamBuildingPost.getContent(),
+                teamBuildingPost.getPlace(),
+                teamBuildingPost.getLimits(),
+                teamBuildingPost.getMember()
+                        .stream().map(UserJoinResponse::fromUserDto)
+                        .collect(Collectors.toList()),
+                teamBuildingPost.getRegisteredAt()
+        );
+    }
+
+}
