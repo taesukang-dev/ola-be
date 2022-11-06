73de7d8f46476422acee6d39d6949e6e91075776
mtae616 <mtae616@naver.com>
2022-11-06 17:09:46 +0900

    add comment domain

---
+++ src/main/java/com/example/ola/domain/Comment.java
@@ -0,0 +1,78 @@
+package com.example.ola.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE comment SET DELETED_AT = NOW() where id = ?")
+@Where(clause = "deleted_at is null")
+@Table(indexes = {@Index(name = "post_id_idx", columnList = "post_id")})
+@Entity
+public class Comment {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "post_id")
+    private Post post;
+    private String content;
+    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "parent_id")
+    private Comment parent;
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    private List<Comment> child = new ArrayList<>();
+    @Column(name = "registered_at") private Timestamp registeredAt;
+    @Column(name = "updated_at") private Timestamp updatedAt;
+    @Column(name = "deleted_at") private Timestamp deletedAt;
+
+
+    public void addChild(Comment comment) {
+        this.child.add(comment);
+    }
+
+    public Comment(User user, Post post, String content) {
+        this.user = user;
+        this.post = post;
+        this.content = content;
+    }
+
+    public Comment(User user, TeamBuildingPost post, String content) {
+        this.user = user;
+        this.post = (TeamBuildingPost)post;
+        this.content = content;
+    }
+
+    public Comment(User user, Post post, String content, Comment parent) {
+        this.user = user;
+        this.post = post;
+        this.content = content;
+        this.parent = parent;
+    }
+
+    public Comment(User user, TeamBuildingPost post, String content, Comment parent) {
+        this.user = user;
+        this.post = (TeamBuildingPost)post;
+        this.content = content;
+        this.parent = parent;
+    }
+
+    public static Comment of(User user, Post post, String content) {
+        return new Comment(user, post, content);
+    }
+
+    public static Comment of(User user, Post post, String content, Comment parent) {
+        return new Comment(user, post, content, parent);
+    }
+
+    @PrePersist void registeredAt() { this.registeredAt = Timestamp.from(Instant.now()); }
+    @PreUpdate void updatedAt() { this.updatedAt = Timestamp.from(Instant.now()); }
+}
