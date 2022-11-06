d9db1af2a09b570bb668f3a14c51bed933d9af3b
mtae616 <mtae616@naver.com>
2022-11-06 19:43:52 +0900

    add alarm entity

---
+++ src/main/java/com/example/ola/domain/Alarm.java
@@ -0,0 +1,43 @@
+package com.example.ola.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.context.annotation.Primary;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE alarm SET DELETED_AT = NOW() where id = ?")
+@Where(clause = "deleted_at is null")
+@Entity
+public class Alarm {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "user_id")
+    private User user;
+    @Embedded AlarmArgs args;
+    @Enumerated(EnumType.STRING) AlarmType alarmType;
+    @Column(name = "registered_at") private Timestamp registeredAt;
+    @Column(name = "updated_at") private Timestamp updatedAt;
+    @Column(name = "deleted_at") private Timestamp deletedAt;
+
+    public Alarm(User user, AlarmArgs args, AlarmType alarmType) {
+        this.user = user;
+        this.args = args;
+        this.alarmType = alarmType;
+    }
+
+    public static Alarm of(User user, AlarmArgs args, AlarmType alarmType) {
+        return new Alarm(user, args, alarmType);
+    }
+
+    @PrePersist void registeredAt() { this.registeredAt = Timestamp.from(Instant.now()); }
+    @PreUpdate void updatedAt() { this.updatedAt = Timestamp.from(Instant.now()); }
+
+}
