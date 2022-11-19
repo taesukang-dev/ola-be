a403958b6767b10dbcb1ade7af9024a0543d38f5
Tae-Su Kang <mtae616@naver.com>
2022-11-19 16:51:10 +0900

    split to other controllers

--- src/main/java/com/example/ola/controller/PostController.java
+++ src/main/java/com/example/ola/controller/PostController.java
@@ -26,25 +26,11 @@ public class PostController {
                 .collect(Collectors.toList()));
     }
 
-    @GetMapping("/team")
-    public Response<List<TeamPostResponse>> getMyTeamPosts(@AuthenticationPrincipal UserPrincipal userPrincipal) {
-        return Response.success(postService.findTeamPostByUsername(userPrincipal.getUsername())
-                .stream().map(TeamPostResponse::fromTeamPostDto)
-                .collect(Collectors.toList()));
-    }
-
     @PostMapping
     public Response<PostResponse> write(
             @RequestBody PostWriteRequest postWriteRequest,
             @AuthenticationPrincipal UserPrincipal userPrincipal) {
         return Response.success(PostResponse.fromPostDto(postService.write(postWriteRequest, userPrincipal.getUsername())));
-    }
-
-    @PostMapping("/team")
-    public Response<TeamPostResponse> writeTeamPost(
-            @RequestBody TeamPostWriteRequest teamPostWriteRequest,
-            @AuthenticationPrincipal UserPrincipal userPrincipal) {
-        return Response.success(TeamPostResponse.fromTeamPostDto(postService.writeTeamPost(teamPostWriteRequest, userPrincipal.getUsername())));
     }
 
     @PutMapping
@@ -54,101 +40,11 @@ public class PostController {
         return Response.success(PostResponse.fromPostDto(postService.updatePost(postUpdateRequest, userPrincipal.getUsername())));
     }
 
-    @PutMapping("/team")
-    public Response<TeamPostResponse> updateTeamPost(
-            @RequestBody TeamPostUpdateRequest teamPostUpdateRequest,
-            @AuthenticationPrincipal UserPrincipal userPrincipal) {
-        return Response.success(TeamPostResponse.fromTeamPostDto(postService.updateTeamPost(teamPostUpdateRequest, userPrincipal.getUsername())));
-    }
-
     @DeleteMapping("/{postId}")
     public Response<Void> removePost(
             @PathVariable Long postId,
             @AuthenticationPrincipal UserPrincipal userPrincipal) {
         postService.delete(postId, userPrincipal.getUsername());
-        return Response.success();
-    }
-
-    @DeleteMapping("/team/{postId}")
-    public Response<Void> removeTeamPost(
-            @PathVariable Long postId,
-            @AuthenticationPrincipal UserPrincipal userPrincipal) {
-        postService.removeTeamPost(postId, userPrincipal.getUsername());
-        return Response.success();
-    }
-
-    @PostMapping("/team/{postId}/member")
-    public Response<Void> addMember(
-            @PathVariable Long postId,
-            @AuthenticationPrincipal UserPrincipal userPrincipal) {
-        postService.addMember(postId, userPrincipal.getUsername());
-        return Response.success();
-    }
-
-    @DeleteMapping("/team/{postId}/member/{memberId}")
-    public Response<Void> deleteMember(
-            @PathVariable Long postId,
-            @PathVariable Long memberId,
-            @AuthenticationPrincipal UserPrincipal userPrincipal) {
-        postService.removeTeamMember(postId, memberId, userPrincipal.getUsername());
-        return Response.success();
-    }
-
-    @GetMapping("/team/{postId}/confirm")
-    public Response<Void> confirmTeam(
-            @PathVariable Long postId,
-            @AuthenticationPrincipal UserPrincipal userPrincipal) {
-        postService.confirmTeam(postId, userPrincipal.getUsername());
-        return Response.success();
-    }
-
-    @GetMapping("/{postId}/comments")
-    public Response<List<CommentResponse>> commentList(
-            @PathVariable Long postId,
-            @AuthenticationPrincipal UserPrincipal userPrincipal) {
-        return Response.success(postService.commentList(postId)
-                .stream().map(CommentResponse::fromCommentDto)
-                .collect(Collectors.toList()));
-    }
-
-    @PostMapping("/{postId}/comments")
-    public Response<Void> writeComment(
-            @RequestBody CommentWriteRequest commentWriteRequest,
-            @PathVariable Long postId,
-            @AuthenticationPrincipal UserPrincipal userPrincipal) {
-        postService.writeComment(postId, userPrincipal.getUsername(), commentWriteRequest.getContent(), commentWriteRequest.getType());
-        return Response.success();
-    }
-
-    @PostMapping("/{postId}/{parentId}/comments")
-    public Response<Void> writeCommentWithParent(
-            @RequestBody CommentWriteRequest commentWriteRequest,
-            @PathVariable Long postId,
-            @PathVariable Long parentId,
-            @AuthenticationPrincipal UserPrincipal userPrincipal) {
-        postService.writeComment(postId, parentId, userPrincipal.getUsername(), commentWriteRequest.getContent(), commentWriteRequest.getType());
-        return Response.success();
-    }
-
-    @DeleteMapping("/{postId}/{commentId}/comments")
-    public Response<Void> deleteComments(
-            @PathVariable Long postId,
-            @PathVariable Long commentId,
-            @AuthenticationPrincipal UserPrincipal userPrincipal) {
-        postService.deleteComment(postId, userPrincipal.getUsername(), commentId);
-        return Response.success();
-    }
-
-    @GetMapping("/alarms")
-    public Response<List<AlarmResponse>> alarmList(@AuthenticationPrincipal UserPrincipal userPrincipal) {
-        return Response.success(postService.alarms(userPrincipal.getUsername())
-                .stream().map(AlarmResponse::fromAlarmDto)
-                .collect(Collectors.toList()));
-    }
-
-    @DeleteMapping("/alarms/{alarmId}")
-    public Response<Void> deleteAlarm(@PathVariable Long alarmId) {
-        postService.deleteAlarm(alarmId);
         return Response.success();
     }
 }
