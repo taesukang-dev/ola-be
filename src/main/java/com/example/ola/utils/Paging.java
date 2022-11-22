27cf531df6394708729bb8c1c3453ff475b0ff93
mtae616 <mtae616@naver.com>
2022-11-22 22:00:40 +0900

    fix page button list

--- src/main/java/com/example/ola/utils/Paging.java
+++ src/main/java/com/example/ola/utils/Paging.java
@@ -10,6 +10,11 @@ public class Paging {
         int total = (int) Math.ceil((float) elementCount / postSize);
         int startNumber = Math.max(currentPage - (PAGE_LIST_NUM / 2), 0);
         int endNumber = Math.min(startNumber + PAGE_LIST_NUM, total);
-        return IntStream.range(startNumber, endNumber).boxed().collect(Collectors.toList());
+        List<Integer> pageList = IntStream.range(startNumber, endNumber).boxed().collect(Collectors.toList());
+        if (pageList.size() < 5) {
+            int start = 5 - pageList.size();
+            return IntStream.range(startNumber - start, endNumber).boxed().collect(Collectors.toList());
+        }
+        return pageList;
     }
 }
