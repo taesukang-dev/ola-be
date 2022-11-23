03cbf8eae164fda4776c3f41fa0f429ae861654e
mtae616 <mtae616@naver.com>
2022-11-23 21:47:43 +0900

    add home gym domain when user update and join

--- src/main/java/com/example/ola/service/UserService.java
+++ src/main/java/com/example/ola/service/UserService.java
@@ -1,13 +1,16 @@
 package com.example.ola.service;
 
+import com.example.ola.domain.HomeGym;
 import com.example.ola.domain.User;
 import com.example.ola.domain.UserRole;
 import com.example.ola.dto.UserDto;
+import com.example.ola.dto.request.HomeGymRequest;
 import com.example.ola.dto.request.UserRequest;
 import com.example.ola.dto.request.UserUpdateRequest;
 import com.example.ola.exception.ErrorCode;
 import com.example.ola.exception.OlaApplicationException;
 import com.example.ola.jwt.JwtTokenProvider;
+import com.example.ola.repository.HomeGymRepository;
 import com.example.ola.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +27,7 @@ import java.util.List;
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final HomeGymRepository homeGymRepository;
     private final JwtTokenProvider jwtTokenProvider;
 
     @Transactional
@@ -40,8 +44,36 @@ public class UserService {
                                 userRequest.getNickname(),
                                 userRequest.getName(),
                                 userRequest.getAgeRange(),
-                                userRequest.getHomeGym(),
+                                checkDuplicateHomeGymAndGetHomeGym(userRequest.getHomeGymRequest()),
                                 userRequest.getGender())));
+    }
+
+    @Transactional
+    public UserDto updateUser(String username, UserUpdateRequest updateParam) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new OlaApplicationException(ErrorCode.USER_NOT_FOUND));
+        user.updateUser(updateParam.getName(), updateParam.getNickname(), checkDuplicateHomeGymAndGetHomeGym(updateParam.getHomeGymRequest()), updateParam.getImgUri());
+        return UserDto.fromUser(user);
+    }
+
+    private HomeGym checkDuplicateHomeGymAndGetHomeGym(HomeGymRequest updateParam) {
+        HomeGym homeGym = homeGymRepository.findByPlaceName(updateParam.getPlaceName())
+                .orElseGet(() -> saveAndReturnHomeGym(updateParam));
+        // 사명은 같아도 주소가 다른 경우
+        if (!homeGym.getRoadAddressName().equals(updateParam.getRoadAddressName())) {
+            homeGym = saveAndReturnHomeGym(updateParam);
+        }
+        return homeGym;
+    }
+
+    private HomeGym saveAndReturnHomeGym(HomeGymRequest homeGymRequest) {
+        return homeGymRepository.save(
+                HomeGym.of(
+                        homeGymRequest.getPlaceName(),
+                        homeGymRequest.getRoadAddressName(),
+                        homeGymRequest.getCategoryName(),
+                        homeGymRequest.getX(),
+                        homeGymRequest.getY()));
     }
 
     public String login(String username, String password) {
@@ -50,14 +82,6 @@ public class UserService {
             throw new OlaApplicationException(ErrorCode.INVALID_PASSWORD);
         }
         return jwtTokenProvider.createToken(username, List.of(UserRole.USER.name()));
-    }
-
-    @Transactional
-    public UserDto updateUser(String username, UserUpdateRequest updateParam) {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new OlaApplicationException(ErrorCode.USER_NOT_FOUND));
-        user.updateUser(updateParam.getName(), updateParam.getNickname(), updateParam.getHomeGym(), updateParam.getImgUri());
-        return UserDto.fromUser(user);
     }
 
     public UserDto findByUsername(String username) {
